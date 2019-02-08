254c8e27026e42b5af0a797c369255072b08c84a
Charlie Phan <charliegphan@gmail.com>
2019-02-08 13:21:43 -0800

    app running, add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: "airbnb",
+  rules: {
+    "no-console": 1,
+    "no-unused-vars": 1,
+    "react/jsx-one-expression-per-line": 0,
+    "import/extensions": 0,
+  },
+  env: {
+    "browser": true
+  }
+}
