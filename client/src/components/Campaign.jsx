132bc191852fd7319be35221546432a6f7525b0d
Charlie Phan <charliegphan@gmail.com>
2019-02-09 07:42:05 -0800

    add Options component

--- client/src/components/Campaign.jsx
+++ client/src/components/Campaign.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
-const Campaign = () => {
-  return (
-    <tr>
-      <td>Campaign ID</td>
-      <td>Spend</td>
-      <td>Revenue</td>
-      <td>Impressions</td>
-      <td>Clicks</td>
-    </tr>
-  );
-};
+const Campaign = () => (
+  <tr>
+    <td>Campaign ID</td>
+    <td>Spend</td>
+    <td>Revenue</td>
+    <td>Impressions</td>
+    <td>Clicks</td>
+  </tr>
+);
+
 
 export default Campaign;
--- client/src/components/CampaignDisplay.jsx
+++ client/src/components/CampaignDisplay.jsx
@@ -5,23 +5,22 @@ import Campaign from './Campaign.jsx';
 import styles from '../../../styles/CampaignDisplay.css';
 
 
-const CampaignDisplay = () => {
-  return (
-    <div className={styles.wrapper}>
-      <table>
-        <tr>
-          <th>Campaign ID</th>
-          <th>Revenue</th>
-          <th>Spend</th>
-          <th>Profit</th>
-          <th>Impressions</th>
-          <th>Clicks</th>
-          <th>Trend</th>
-        </tr>
-        <Campaign />
-      </table>
-    </div>
-  );
-};
+const CampaignDisplay = () => (
+  <div className={styles.wrapper}>
+    <table>
+      <tr>
+        <th>Campaign ID</th>
+        <th>Revenue</th>
+        <th>Spend</th>
+        <th>Profit</th>
+        <th>Impressions</th>
+        <th>Clicks</th>
+        <th>Trend</th>
+      </tr>
+      <Campaign />
+    </table>
+  </div>
+);
+
 
 export default CampaignDisplay;
